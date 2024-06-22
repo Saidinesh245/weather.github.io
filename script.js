3024c09ac9fcb97580954ba32be513ea70e8c8ea
Saidinesh245 <100748998+Saidinesh245@users.noreply.github.com>
2024-06-21 22:44:29 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const apiKey="413f4d244c045fc9f9ab4ac4432d6746";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric";
+document.getElementById("weather-icon-box").innerHTML="";
+
+async function checkWeather(){
+    var city = document.getElementById("input-field").value;
+
+    if(city==""){
+        city="toronto";
+        document.getElementById("input-field").value="Toronto";
+    }
+    
+    const response = await fetch(apiUrl + `&appid=${apiKey}` + `&q=${city}`);  // change this usig the'$' 
+    var data = await response.json();
+    var description = data.weather[0].main;
+
+    console.log(data);
+
+    var temprature = Math.round(data.main.temp);
+    document.querySelector(".weather-temp").innerHTML = temprature + " °C";
+    document.querySelector(".weather-note").innerHTML = data.weather[0].main ;
+
+    if(description == "Clouds"){
+        document.querySelector(".weather-icon").src="Weather-icons/cloud.png";
+    }
+    else if(description == "Clear"){
+        document.querySelector(".weather-icon").src="Weather-icons/day-and-night.png";
+    }
+    else if(description == "Rain"){
+        document.querySelector(".weather-icon").src="Weather-icons/rain.png";
+    }
+    else if(description == "Snow"){
+        document.querySelector(".weather-icon").src="Weather-icons/snow.png";
+    }
+    else if(description== "Haze"){
+        document.querySelector(".weather-icon").src="Weather-icons/haze.png"
+    }
+    else if(description== "Mist"){
+        document.querySelector(".weather-icon").src="Weather-icons/mist.png"
+    }
+}
+checkWeather();
